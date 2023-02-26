da080aa1a1df95bf97aa3710ad8b2fa8c32105f6
Andreson26 <62575538+Andreson26@users.noreply.github.com>
2023-02-26 00:30:05 -0500

    generate the basic header elements and footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Layout({ children, title }) {
+    const [year, setYear] = useState();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setYear(new Date().getFullYear())
+        }, 1000)
+        return () => clearInterval(interval)
+    }, [])
+
   return (
     <>
       <Head>
@@ -12,11 +22,27 @@ export default function Layout({ children, title }) {
       </Head>
 
       <div className="flex min-h-screen flex-col justify-between">
-        <header>Header</header>
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <p className="text-lg font-bold">ANT Ecommerce</p>
+            </Link>
+            <div className="flex">
+              <Link href="/cart">
+                <p className="p-2">Cart</p>
+              </Link>
+              <Link href="/login">
+                <p className="p-2">Login</p>
+              </Link>
+            </div>
+          </nav>
+        </header>
 
-        <main>{children}</main>
+        <main className="container m-auto mt-4 px-4">{children}</main>
 
-        <footer>footer</footer>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © {year}  ANT Ecommerce</p>
+          </footer>
       </div>
     </>
   );
