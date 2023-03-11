252faafb17782348a5191b4026d2686a35a17d90
Andreson26 <62575538+Andreson26@users.noreply.github.com>
2023-03-11 03:41:40 -0500

    created the place order component

--- components/CheckoutWizard.js
+++ components/CheckoutWizard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function CheckouWizard({ activeStep = 0 }) {
+export default function CheckoutWizard({ activeStep = 0 }) {
   return (
     <div className="mb-5 flex flex-wrap">
       {["User Login", "Shipping Address", "Payment Method", "Place Order"].map(
--- pages/api/seed.js
+++ pages/api/seed.js
@@ -5,7 +5,6 @@ import data from "@/utils/data";
 
 
 const handler = async (req, res) => {
-  console.log(Product)
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
